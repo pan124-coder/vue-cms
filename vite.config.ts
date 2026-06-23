88feed87146fe89a7ddc387be73d9a0a3e7f119f
潘伟羽 <2832580020@qq.com>
2026-06-23 16:33:44 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  base: '/admin/', // 保留你的子目录配置不要改
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+    extensions: ['.js', '.ts', '.vue', '.json', '.css']
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8081',
-        changeOrigin: true,
-      }
-    }
+  // 关键：强制预构建wangeditor依赖，避免线上裸模块解析失败
+  optimizeDeps: {
+    include: [
+      '@wangeditor/editor'
+    ]
   }
 })
