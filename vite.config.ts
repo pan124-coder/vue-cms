bdc2cf1de05d7a908f12708ca9a2cef639c5bd2e
潘伟羽 <2832580020@qq.com>
2026-06-23 16:41:35 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// 临时注释devtools消除版本冲突
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  base: '/admin/', // 保留你的子目录配置不要改
+  plugins: [vue()],
+  base: '/admin/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+    extensions: ['.js', '.ts', '.vue', '.json', '.css']
   },
+  // 强制预构建，让Rolldown识别第三方包
+  optimizeDeps: {
+    include: ['@wangeditor/editor']
+  },
+  build: {
+    rollupOptions: {
+      // 不要外部化该依赖，需要打包进产物
+      external: [],
+      onwarn(warning, warn) {
+        if (warning.code === 'UNRESOLVED_IMPORT') return
+        warn(warning)
+      }
+    }
+  }
 })
